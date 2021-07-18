6830b3b0f7c627c16fa067a1fc38d3abbe42cfec
thatclassygirl <citlalli.villegas@icloud.com>
2021-07-18 14:52:25 -0500

    Vacunas document.write

--- Clases Códigos/Intro a Javascript/02. Vacuna/main.js
+++ Clases Códigos/Intro a Javascript/02. Vacuna/main.js
@@ -6,18 +6,18 @@ Para que yo me pueda vacunar, necesito cumplir alguno de los siguientes requisit
 */
 
 // Paso #1: Definir las variables que necesitamos
-var edad = 28;
-var fronterizo = true;
-var embarazada = false;
-var semanasGestacion = 0;
+var edad = parseInt(prompt("¿Qué edad tienes?"));
+var fronterizo = prompt("¿Vives en una ciudad fronteriza del Norte del País? - si o no").toLowerCase();
+var embarazada = prompt("¿Estás embarazada? si o no").toLowerCase();
+var semanasGestacion = parseInt(prompt("En caso de estar embarazada, ¿cuántos meses tieness? Si no es el caso, contesta cero"));
 
 // Paso #2: Validar la primera condición
-if (edad >= 18 && fronterizo == true){
-    console.log("Si te puedes vacunar, por ser mayor de 18 años y vivir en un municipio fronterizo del Norte");
-} else if (edad >= 18 && embarazada == true && semanasGestacion >=9){
-    console.log("Si te puedes vacunar, por estar embarazada y con 9 o más semanas de gestación");
+if (edad >= 18 && fronterizo == "ai"){
+    document.write("Si te puedes vacunar, por ser mayor de 18 años y vivir en un municipio fronterizo del Norte");
+} else if (edad >= 18 && embarazada == "si" && semanasGestacion >=9){
+    document.write("Si te puedes vacunar, por estar embarazada y con 9 o más semanas de gestación");
 } else if (edad >= 30){
-    console.log("Si te puedes vacunar, por tener 30 años o más");
+    document.write("Si te puedes vacunar, por tener 30 años o más");
 } else {
-    console.log("Lo sentimos, aún no te puedes vaunar, espera próximos avisos");
+    document.write("Lo sentimos, aún no te puedes vaunar, espera próximos avisos");
 }
